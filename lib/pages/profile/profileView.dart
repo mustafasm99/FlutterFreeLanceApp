1836cd8d93b421aed723b124d34bbf5f56df4364
beshiraltai <Altaibeshir@gmail.com>
2024-11-23 12:30:38 +0300

    complete profile page

--- lib/pages/profile/profileView.dart
+++ lib/pages/profile/profileView.dart
@@ -39,6 +39,256 @@ class ProfileView extends StatelessWidget {
                     }
                   },
                 ),
+                Container(
+                    width: 345,
+                    height: 192,
+                    margin: EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFECF6F9),
+                      borderRadius: BorderRadius.circular(
+                          15), // Add this line for rounded corners
+                    ),
+                    child: Padding(
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+                      child: Column(
+                        children: [
+                          ///Education
+                          Row(
+                            children: [
+                              Text(
+                                'Education',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              Spacer(),
+                              // SvgPicture.asset('SvgIcon/09_add-01.svg'),
+                              Icon(Icons.add),
+                              SvgPicture.asset('SvgIcon/08_edit-02.svg')
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset('Assets/Image/uni.png'),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('University of Baghdad',
+                                      style: TextStyle(
+                                          fontSize: 16, fontWeight: FontWeight.bold)),
+                                  Text('22 jan 2020 - 11 may 2024.',
+                                      style: TextStyle(
+                                          fontSize: 12, color: Color(0xff999999))),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    'Information & Communication\nEngineering, Al-Khwarizmi College',
+                                    style: TextStyle(
+                                        fontSize: 14, fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
+                SizedBox(height: 10,),
+                ///Skills Container
+                Container(
+                    width: 345,
+                    height: 264,
+                    margin: EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFECF6F9),
+                      borderRadius: BorderRadius.circular(
+                          15), // Add this line for rounded corners
+                    ),
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 20),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'skills',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: 20),
+                                ),
+                                Spacer(flex: 6,),
+                                // SvgPicture.asset('SvgIcon/09_add-01.svg'),
+                                Icon(Icons.add),
+                                Spacer(),
+                                SvgPicture.asset('SvgIcon/08_edit-02.svg'),
+
+                              ],
+                            ),
+                            SizedBox(height: 20,),
+
+                            ///Skills Rows
+                            Row(
+                              children: [
+                                SvgPicture.asset('SvgIcon/user-multiple-02.svg'),
+                                SizedBox(width: 10,),
+                                Text('figma (Software)',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),)
+                              ],
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              width: 345,
+                              height: 1,
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Color(0xffD9D9D9),
+                                          width: 1
+                                      )
+                                  )
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            ///Skills Row
+                            Row(
+                              children: [
+                                SvgPicture.asset('SvgIcon/user-multiple-02.svg'),
+                                SizedBox(width: 10,),
+                                Text('figma (Software)',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),)
+                              ],
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              width: 345,
+                              height: 1,
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Color(0xffD9D9D9),
+                                          width: 1
+                                      )
+                                  )
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+
+                            ///Skills Row
+                            Row(
+                              children: [
+                                SvgPicture.asset('SvgIcon/user-multiple-02.svg'),
+                                SizedBox(width: 10,),
+                                Text('figma (Software)',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),)
+                              ],
+                            ),
+
+                          ],
+                        ))),
+                SizedBox(height: 20,),
+
+
+                ///Course & Certification
+                Container(
+                  width: 345,
+                  height: 400,
+                  margin: EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFECF6F9),
+                    borderRadius: BorderRadius.circular(
+                        15), // Add this line for rounded corners
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Course &\nCertification',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            Spacer(flex: 6,),
+                            // SvgPicture.asset('SvgIcon/09_add-01.svg'),
+                            Icon(Icons.add),
+                            Spacer(),
+                            SvgPicture.asset('SvgIcon/08_edit-02.svg'),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset('Assets/Image/microsoft.png'),
+                            SizedBox(width: 20,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Microsoft 365 Certified:\nFudamentals',
+                                    style: TextStyle(
+                                        fontSize: 16, fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10,),
+                                Text('Microsoft\nissued in 22 jul 2024',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Color(0xff999999))),
+
+
+
+                              ],
+                            ),
+
+
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Container(
+                          width: 345,
+                          height: 1,
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      color: Color(0xffD9D9D9),
+                                      width: 1
+                                  )
+                              )
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset('Assets/Image/microsoft.png'),
+                            SizedBox(width: 20,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Microsoft 365 Certified:\nFudamentals',
+                                    style: TextStyle(
+                                        fontSize: 16, fontWeight: FontWeight.bold)),
+                                SizedBox(height: 10,),
+                                Text('Microsoft\nissued in 22 jul 2024',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Color(0xff999999))),
+
+
+
+                              ],
+                            ),
+
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ],
             )),
       ),
